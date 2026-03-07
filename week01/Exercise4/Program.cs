88d0b2a6129f02b659e5c89813cd035e79828bd9
miguelerian <mavalospriante@byupathway.edu>
2026-03-07 16:53:49 -0600

    lists learning activity

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        List<int> numbers = new List<int>();
+
+        int number = -1;
+        while (number != 0)
+
+        {
+            Console.Write("Enter a number: ");
+            string user = Console.ReadLine();
+            number = int.Parse(user);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        }
+
+        int sum = 0;
+        foreach (int i in numbers)
+        {
+            sum += i;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is {average}");
+
+        int max = numbers[0];
+
+        foreach (int i in numbers)
+        {
+            if (i > max)
+            {
+                max = i;
+            }
+        }
+
+        Console.WriteLine($"The max is {max}");
     }
 }
