86a5598b1d0c143a018f50ead11eb4b9674c934e
miguelerian <mavalospriante@byupathway.edu>
2026-04-04 13:04:04 -0600

    inheritance activity

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        static void Main(string[] args)
+        {
+            // Base Assignment test
+            Assignment a1 = new Assignment("Eduardo Lima", "Multiplication");
+            Console.WriteLine(a1.GetSummary());
+            Console.WriteLine();
+
+            // Math Assignment test
+            MathAssignment math = new MathAssignment(
+                "Miguel Avalos",
+                "Fractions",
+                "7.3",
+                "8-19"
+            );
+
+            Console.WriteLine(math.GetSummary());
+            Console.WriteLine(math.GetHomeworkList());
+            Console.WriteLine();
+
+            // Writing Assignment test
+            WritingAssignment writing = new WritingAssignment(
+                "Benito Juarez",
+                "European Economy",
+                "The Causes of World War"
+            );
+
+            Console.WriteLine(writing.GetSummary());
+            Console.WriteLine(writing.GetWritingInformation());
+        }
     }
 }
